474b5d88755e455bb0a76c7d17d369272a50b1c2
Pavel Alekseev <pavelbooking.com@yandex.ru>
2023-04-24 14:37:39 +0500

    каркас готов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from "react";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<body>
+				<main>
+					<div className="outputContainer">
+						<h1 className="output"></h1>
+					</div>
+					<div id="buttons" className="button-row">
+						<Button name="number" value={"AC"} />
+						<Button name="number" value={"+/-"} />
+						<Button name="number" value={"%"} />
+            <Button name="delete" value={""} />
+					</div>
+					<div id="buttons" className="button-row">
+						<Button name="number" value={7} />
+						<Button name="number" value={8} />
+						<Button name="number" value={9} />
+            <Button name="operation" value={"÷"} />						
+					</div>
+					<div id="buttons" className="button-row">
+						<Button name="number" value={4} />
+						<Button name="number" value={5} />
+						<Button name="number" value={6} />
+            <Button name="operation" value={"×"} />						
+					</div>
+					<div id="buttons" className="button-row">
+						<Button name="number" value={1} />
+						<Button name="number" value={2} />
+						<Button name="number" value={3} />
+            <Button name={"operation"} value={"−"} />
+						
+					</div>
+					<div id="buttons" className="button-row">
+						<Button name="number" value={"0"} />
+						<Button name="number" value={"."} />
+						<Button name="number" value={"="} />
+            <Button name="operation" value={"+"} />
+					</div>
+				</main>
+			</body>
+		</>
+	);
 }
 
 export default App;
